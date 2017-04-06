b257ed782ac81363a7f7c76aeef91bb40d918fc9
balintvecsey <balint.vecsey@gmail.com>
2017-04-06 09:51:45 +0200

    inital commit

---
+++ ToDoApp/src/App.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Bálint on 2017. 04. 06..
+ */
+public class App {
+
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ ToDoApp/src/ToDo.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Bálint on 2017. 04. 06..
+ */
+public class ToDo {
+
+}
