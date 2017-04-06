2976a003e123fb6e3d2e0b3c6d880c5863e94617
balintvecsey <balint.vecsey@gmail.com>
2017-04-06 15:40:26 +0200

    made list task

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,9 +1,3 @@
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
 /**
  * Created by Bálint on 2017. 04. 06..
  */
@@ -11,6 +5,6 @@ public class App {
 
   public static void main(String[] args) {
     ArgumentHandler argument = new ArgumentHandler(args);
-    argument.printUsage();
+    argument.arguments();
   }
 }
--- ToDoApp/src/ArgumentHandler.java
+++ ToDoApp/src/ArgumentHandler.java
@@ -1,20 +1,24 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by Bálint on 2017. 04. 06..
  */
 public class ArgumentHandler {
+
   String[] args;
 
-  public ArgumentHandler(String[] args){
+  public ArgumentHandler(String[] args) {
     this.args = args;
   }
 
-  public void printUsage() {
-    if (args.length == 0){
+  public void arguments() {
+    if (args.length == 0) {
       try {
         Path path = Paths.get("../files/usage.txt");
         List<String> lines = Files.readAllLines(path);
@@ -24,6 +28,25 @@ public class ArgumentHandler {
         }
       } catch (Exception e) {
         System.out.println("Error: " + e.getClass());
+      }
+    } else if (Arrays.toString(args).contains("-l")) {
+      List<String> content = new ArrayList<>();
+      content.add("Walk the dog");
+      content.add("Buy milk");
+      content.add("Do homework");
+      int num = 0;
+
+      try {
+        Path path = Paths.get("../files/todolist.txt");
+        Files.write(path, content);
+        List<String> lines = Files.readAllLines(path);
+
+        for (String line : lines) {
+          num++;
+          System.out.println(num + " - " + line);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
       }
     }
   }
