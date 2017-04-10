d2e77d993265d222be194098538b8715c97100c2
balintvecsey <balint.vecsey@gmail.com>
2017-04-10 09:11:25 +0200

    refactor checkTask and add some colors

--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -8,7 +8,12 @@ import java.util.List;
  * Created by Bálint on 2017. 04. 06..
  */
 public class ToDoList {
+
   String[] args;
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+  public static final String ANSI_CYAN = "\u001B[36m";
 
   public ToDoList(String[] args) {
     this.args = args;
@@ -20,7 +25,7 @@ public class ToDoList {
       List<String> lines = Files.readAllLines(path);
 
       for (String usage : lines) {
-        System.out.println(usage);
+        System.out.println(ANSI_CYAN + usage + ANSI_RESET);
       }
     } catch (Exception e) {
       System.out.println("Error: " + e.getClass());
@@ -33,7 +38,7 @@ public class ToDoList {
     }
   }
 
-  public void showList () {
+  public void showList() {
     if (args[0].contains("-l")) {
       try {
         Path path = Paths.get("../files/todolist.txt");
@@ -46,7 +51,11 @@ public class ToDoList {
         } else {
           for (String line : lines) {
             num++;
-            System.out.println(num + " - " + line);
+            if (line.startsWith("[ ")) {
+              System.out.println(ANSI_RED + num + " - " + line + ANSI_RESET);
+            } else {
+              System.out.println(ANSI_GREEN + num + " - " + line + ANSI_RESET);
+            }
           }
         }
       } catch (Exception e) {
@@ -56,48 +65,62 @@ public class ToDoList {
   }
 
   public void addTask() {
-    if (args[0].contains("-a") && args.length > 1) {
-      String task = args[1];
-      String undone = "[ ] ";
-      try {
-        Path path = Paths.get("../files/todolist.txt");
-        List<String> lines = Files.readAllLines(path);
+    if (args[0].contains("-a")) {
+      if (args.length > 1) {
+        String task = args[1];
+        String undone = "[ ] ";
+        try {
+          Path path = Paths.get("../files/todolist.txt");
+          List<String> lines = Files.readAllLines(path);
 
-        lines.add(undone + task);
-        Files.write(path, lines);
-      } catch (Exception e) {
-        System.out.println("Error: " + e.getClass());
+          lines.add(undone + task);
+          Files.write(path, lines);
+        } catch (Exception e) {
+          System.out.println("Error: " + e.getClass());
+        }
+      } else if (args.length == 1) {
+        System.out.println("Unable to add: no task provided");
       }
-    } else if (args.length == 1){
-      System.out.println("Unable to add: no task provided");
     }
   }
 
   public void removeTask() {
     if (args[0].contains("-r")) {
-      try {
-        Path path = Paths.get("../files/todolist.txt");
-        List<String> lines = Files.readAllLines(path);
-
-        lines.remove(Integer.parseInt(args[1]) - 1);
-        Files.write(path, lines);
-      } catch (Exception e) {
-        System.out.println("Error: " + e.getClass());
+      if (args.length > 1) {
+        try {
+          Path path = Paths.get("../files/todolist.txt");
+          List<String> lines = Files.readAllLines(path);
+          
+          lines.remove(Integer.parseInt(args[1]) - 1);
+          Files.write(path, lines);
+        } catch (Exception e) {
+          System.out.println("Error: " + e.getClass());
+        }
+      } else if (args.length == 1) {
+        System.out.println("Unable to remove: no index provided");
       }
     }
   }
 
   public void checkTask() {
     if (args[0].contains("-c")) {
-      try {
-        Path path = Paths.get("../files/todolist.txt");
-        List<String> lines = Files.readAllLines(path);
-        String task = lines.get(Integer.parseInt(args[1]) - 1);
-
-        lines.set(Integer.parseInt(args[1]) - 1, "[x] " + task.substring(4));
-        Files.write(path, lines);
-      } catch (Exception e) {
-        System.out.println("Error: " + e.getClass());
+      if (args.length > 1) {
+        try {
+          Path path = Paths.get("../files/todolist.txt");
+          List<String> lines = Files.readAllLines(path);
+          String task = lines.get(Integer.parseInt(args[1]) - 1);
+          if (task.substring(0, 2).equals("[ ")) {
+            lines.set(Integer.parseInt(args[1]) - 1, "[x" + task.substring(2));
+            Files.write(path, lines);
+          } else {
+            lines.set(Integer.parseInt(args[1]) - 1, "[ " + task.substring(2));
+            Files.write(path, lines);
+          }
+        } catch (Exception e) {
+          System.out.println("Error: " + e.getClass());
+        }
+      } else if (args.length == 1) {
+        System.out.println("Unable to remove: no index provided");
       }
     }
   }
